1bc7d38ae0c4497c54ba021f31dbd56787650733
Muhammad Ammar <57048049+muhamad-ammar@users.noreply.github.com>
2026-06-18 18:32:57 +0500

    Initial Commit

---
+++ PropFinder/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  PropFinder
+//
+//  Created by Muhammad Ammar on 18/06/2026.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
---
+++ PropFinderTests/PropFinderTests.swift
@@ -0,0 +1,19 @@
+//
+//  PropFinderTests.swift
+//  PropFinderTests
+//
+//  Created by Muhammad Ammar on 18/06/2026.
+//
+
+import Testing
+@testable import PropFinder
+
+struct PropFinderTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+        // Swift Testing Documentation
+        // https://developer.apple.com/documentation/testing
+    }
+
+}
